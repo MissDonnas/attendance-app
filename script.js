701b54f3eea0ea48bc73425a2689150f9301b912
MissDonnas <lpilgeram@gmail.com>
2025-08-03 11:24:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Import the Firebase libraries as modules from a CDN
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js';
+import { getFirestore, collection, onSnapshot, updateDoc, doc, serverTimestamp } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDDZYW4AQXyCtkB2mUMN-QIsc57ZwjvlsE",
   authDomain: "attendance-app-1b27e.firebaseapp.com",
@@ -13,9 +10,9 @@ const firebaseConfig = {
   storageBucket: "attendance-app-1b27e.firebasestorage.app",
   messagingSenderId: "1043261702908",
   appId: "1:1043261702908:web:a29f90f3fb11c47476cd79",
-  measurementId: "G-6M0R6HZH5J"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
